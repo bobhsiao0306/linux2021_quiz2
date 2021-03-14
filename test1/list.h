cf71bfb397c3e516830e5012c74f97a937d9ea32
BobHsiao <f74066137@gs.ncku.edu.tw>
2021-03-14 12:14:04 +0800

    Able to run

--- test1/list.h
+++ test1/list.h
@@ -35,32 +35,53 @@ struct list_head {
  * INIT_LIST_HEAD() - Initialize empty list head
  * @head: pointer to list head
  */
-static inline void INIT_LIST_HEAD(struct list_head *head);
+static inline void INIT_LIST_HEAD(struct list_head *head)
+{
+    head->next = head; head->prev = head;
+}
 
 /**
  * list_add_tail() - Add a list node to the end of the list
  * @node: pointer to the new node
  * @head: pointer to the head of the list
  */
-static inline void list_add_tail(struct list_head *node, struct list_head *head);
+static inline void list_add_tail(struct list_head *node, struct list_head *head)
+{
+    struct list_head *prev = head->prev;
+
+    prev->next = node;
+    node->next = head;
+    node->prev = prev;
+    head->prev = node;
+}
 
 /**
  * list_del() - Remove a list node from the list
  * @node: pointer to the node
  */
-static inline void list_del(struct list_head *node);
+static inline void list_del(struct list_head *node)
+{
+    struct list_head *next = node->next, *prev = node->prev;
+    next->prev = prev; prev->next = next;
+}
 
 /**
  * list_empty() - Check if list head has no nodes attached
  * @head: pointer to the head of the list
  */
-static inline int list_empty(const struct list_head *head);
+static inline int list_empty(const struct list_head *head)
+{
+    return (head->next == head);
+}
 
 /**
  * list_is_singular() - Check if list head has exactly one node attached
  * @head: pointer to the head of the list
  */
-static inline int list_is_singular(const struct list_head *head);
+static inline int list_is_singular(const struct list_head *head)
+{
+    return (!list_empty(head) && head->prev == head->next);
+}
 
 /**
  * list_splice_tail() - Add list nodes from a list to end of another list
@@ -68,7 +89,20 @@ static inline int list_is_singular(const struct list_head *head);
  * @head: pointer to the head of the list
  */
 static inline void list_splice_tail(struct list_head *list,
-                                    struct list_head *head);
+                                    struct list_head *head)
+{
+    struct list_head *head_last = head->prev;
+    struct list_head *list_first = list->next, *list_last = list->prev;
+
+    if (list_empty(list))
+        return;
+
+    head->prev = list_last;
+    list_last->next = head;
+
+    list_first->prev = head_last;
+    head_last->next = list_first;
+}
 
 /**
  * list_cut_position() - Move beginning of a list to another list
@@ -78,7 +112,26 @@ static inline void list_splice_tail(struct list_head *list,
  */
 static inline void list_cut_position(struct list_head *head_to,
                                      struct list_head *head_from,
-                                     struct list_head *node);
+                                     struct list_head *node)
+{
+    struct list_head *head_from_first = head_from->next;
+
+    if (list_empty(head_from))
+        return;
+
+    if (head_from == node) {
+        INIT_LIST_HEAD(head_to);
+        return;
+    }
+
+    head_from->next = node->next;
+    head_from->next->prev = head_from;
+
+    head_to->prev = node;
+    node->next = head_to;
+    head_to->next = head_from_first;
+    head_to->next->prev = head_to;
+}
                                      
 /**
  * list_entry() - Calculate address of entry that contains list node
